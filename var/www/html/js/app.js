39250e3df54994ca27acf2cc39e6e3158dfd13ea
Brady Raines <dustin.raines@guideit.com>
2025-02-07 10:52:06 -0600

    building website

---
+++ var/www/html/js/app.js
@@ -0,0 +1,1 @@
+console.log("Website 1 is loaded!");
---
+++ var/www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website 1</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>Welcome to Website 1</h1>
+    <p>This is your first website powered by GitHub and Apache!</p>
+    <script src="js/app.js"></script>
+</body>
+</html>
